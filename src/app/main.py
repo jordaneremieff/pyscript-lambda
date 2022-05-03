f39249eb0323bfc1b19e7a6f8e8371303804a976
Jordan Eremieff <jordan@eremieff.com>
2022-05-03 22:20:55 +1000

    :construction_worker: Allow workflow dispatch for deployment, add pyscript example.

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 from app.conf import DEBUG, LIB_PATH
 
 
 app = FastAPI(debug=DEBUG)
 
+BODY = """
+<html>
+<head>
+    <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+</head>
+<py-env>
+</py-env>
+<body>
+    <py-script>
+        print("Hello, world!")
+    </py-script>
+</body>
+</html>
+"""
 
-@app.get("/")
-def get_lambda_info(request: Request):
-    payload = {
-        "event": request.scope.get("aws.event", {}),
-        "dependencies": sorted([item.name for item in LIB_PATH.iterdir()]),
-    }
 
-    return payload
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return BODY
